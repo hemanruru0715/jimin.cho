415306286b2e1339b51eafb93a1821d014fc222e
조지민 <jimin.cho@hyundai-ite.com>
2024-08-30 15:30:46 +0900

    gfa11223232

--- app/api/frame/route.ts
+++ app/api/frame/route.ts
@@ -159,37 +159,38 @@ import { NEXT_PUBLIC_URL } from '@/app/config';
 async function getResponse(req: NextRequest): Promise<NextResponse> {
   try {
 
-    const apiKey = process.env.AIRSTACK_API_KEY;
-    if (!apiKey) {
-      throw new Error("AIRSTACK_API_KEY is not defined in environment variables");
-    }
-    init(apiKey);
+    // const apiKey = process.env.AIRSTACK_API_KEY;
+    // if (!apiKey) {
+    //   throw new Error("AIRSTACK_API_KEY is not defined in environment variables");
+    // }
+    // init(apiKey);
 
-    const body = await req.json();
-    const { isValid, message } = await validateFramesMessage(body);
+    // const body = await req.json();
+    // const { isValid, message } = await validateFramesMessage(body);
 
-    if (!isValid) {
-      return new NextResponse('Message not valid', { status: 500 });
-    }
+    // if (!isValid) {
+    //   return new NextResponse('Message not valid', { status: 500 });
+    // }
 
-    const myFid = Number(message?.data?.fid) || 0;
-    const input: FarcasterUserDetailsInput = { fid: myFid };
-    const { data, error }: FarcasterUserDetailsOutput = await getFarcasterUserDetails(input);
+    // const myFid = Number(message?.data?.fid) || 0;
+    // const input: FarcasterUserDetailsInput = { fid: myFid };
+    // const { data, error }: FarcasterUserDetailsOutput = await getFarcasterUserDetails(input);
 
-    if (error) throw new Error(error);
+    // if (error) throw new Error(error);
 
-    // return new NextResponse(
-    //   getFrameHtmlResponse({
-    //     buttons: [
-    //       { label: 'abcd' },
-    //       { action: 'link', label: 'link/🔎', target: 'https://onchainkit.xyz' },
-    //       { action: 'link', label: 'Dog pictures', target: 'https://www.naver.com' },
-    //     ],
-    //     image: { src: `${NEXT_PUBLIC_URL}/api/og?fid=${myFid}` },
-    //     postUrl: `${NEXT_PUBLIC_URL}/api/frame`,
-    //     state: { time: new Date().toISOString() },
-    //   })
-    // );
+    return new NextResponse(
+      getFrameHtmlResponse({
+        buttons: [
+          { label: 'abcd' },
+          { action: 'link', label: 'link/🔎', target: 'https://onchainkit.xyz' },
+          { action: 'link', label: 'Dog pictures', target: 'https://www.naver.com' },
+        ],
+        //image: { src: `${NEXT_PUBLIC_URL}/api/og?fid=${myFid}` },
+        image: { src: `${NEXT_PUBLIC_URL}/park-3.png` },
+        postUrl: `${NEXT_PUBLIC_URL}/api/frame`,
+        state: { time: new Date().toISOString() },
+      })
+    );
   } catch (error) {
     console.error('Error processing request:', error);
     return new NextResponse('Internal Server Error', { status: 500 });
