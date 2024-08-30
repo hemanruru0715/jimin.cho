6c3480f745737025484eeb576dde597f99c1bfe4
조지민 <jimin.cho@hyundai-ite.com>
2024-08-30 15:26:28 +0900

    gfd22333

--- app/api/frame/route.ts
+++ app/api/frame/route.ts
@@ -154,7 +154,50 @@ import { NEXT_PUBLIC_URL } from '@/app/config';
 // export const dynamic = 'force-dynamic';
 
 
-export async function POST(req: NextRequest) {
-  // Your logic here
+
+
+async function getResponse(req: NextRequest): Promise<NextResponse> {
+  try {
+
+    const apiKey = process.env.AIRSTACK_API_KEY;
+    if (!apiKey) {
+      throw new Error("AIRSTACK_API_KEY is not defined in environment variables");
+    }
+    init(apiKey);
+
+    const body = await req.json();
+    const { isValid, message } = await validateFramesMessage(body);
+
+    if (!isValid) {
+      return new NextResponse('Message not valid', { status: 500 });
+    }
+
+    const myFid = Number(message?.data?.fid) || 0;
+    const input: FarcasterUserDetailsInput = { fid: myFid };
+    const { data, error }: FarcasterUserDetailsOutput = await getFarcasterUserDetails(input);
+
+    if (error) throw new Error(error);
+
+    // return new NextResponse(
+    //   getFrameHtmlResponse({
+    //     buttons: [
+    //       { label: 'abcd' },
+    //       { action: 'link', label: 'link/🔎', target: 'https://onchainkit.xyz' },
+    //       { action: 'link', label: 'Dog pictures', target: 'https://www.naver.com' },
+    //     ],
+    //     image: { src: `${NEXT_PUBLIC_URL}/api/og?fid=${myFid}` },
+    //     postUrl: `${NEXT_PUBLIC_URL}/api/frame`,
+    //     state: { time: new Date().toISOString() },
+    //   })
+    // );
+  } catch (error) {
+    console.error('Error processing request:', error);
+    return new NextResponse('Internal Server Error', { status: 500 });
+  }
 }
 
+export async function POST(req: NextRequest): Promise<Response> {
+  return getResponse(req);
+}
+
+export const dynamic = 'force-dynamic';
